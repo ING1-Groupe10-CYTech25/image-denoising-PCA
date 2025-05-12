e5f42aa0a0e2fe61ec6bb8afb6f51bddaf795fb0
Slyfti <titout24@outlook.fr>
2025-05-12 16:29:09 +0200

    Test ajout javadoc

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 
 public class Main {
-
+	
+	/**
+	 * Fonction principale
+	 * @author Sylvain CAPBLANCQ
+	 * @param args Arguments passés dans la ligne de commande
+	 * @version 1.0
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("main");
