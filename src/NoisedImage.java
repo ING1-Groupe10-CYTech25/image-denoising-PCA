6ff02b9774dc4733e65893c56d99f539702a4e05
Paul Cousin <p_cousin@outlook.fr>
2025-05-12 17:09:54 +0200

    classe NoisedImage toujours sans comment et faut update l'UML

--- src/NoisedImage.java
+++ src/NoisedImage.java
@@ -1,19 +1,16 @@
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import javax.imageio.ImageIO;
 import java.util.Random;
+
+import javax.imageio.ImageIO;
 //Commentaire
 public class NoisedImage extends Image{
 	private int sigma;
-	public NoisedImage(Image img, int sigma) {
-		try {
-		    this.setSigma(sigma);
-		    noisify(this.getSigma());
-		    this.setName(this.getName() + '_' + sigma);
-		} catch (IOException e) {
-		    e.printStackTrace();
-		}
+	public NoisedImage(String filePath, int sigma) {
+		super(filePath);
+		this.setSigma(sigma);
+		noisify(this.getSigma());
+		this.setName(this.getName() + '_' + sigma);
 	}
 	public void setSigma(int sigma) {
 		this.sigma = sigma;
@@ -22,15 +19,15 @@ public class NoisedImage extends Image{
 		return this.sigma;
 	}
 	public void noisify(int sigma) {
-		int w = this.getImage().getWidth();
-		int h = this.getImage().getHeight();
 		Random r = new Random();
-		for (int y = 0; y < h; y ++) {
-			for (int x = 0; x < w; x ++) {
-				int gray = (int) (this.getRaster().getSample(x, y, 0) + r.nextGaussian() * sigma);
-				gray = (gray > 255 ? 255 : (gray < 0 ? 0 : gray)); // correction pour eviter overflow
-				this.getRaster().setSample(x, y, 0, gray);
+		for (int y = 0; y < this.getHeight(); y ++) {
+			for (int x = 0; x < this.getWidth(); x ++) {
+				this.setPixel(x, y, (int) (this.getPixel(x,y) + r.nextGaussian() * sigma));
 			}
 		}
+	}
+	@Override
+	public void saveImage(String path) {
+	    super.saveImage((path == null ? path : "img") + "/noised" );
 	}
 }
