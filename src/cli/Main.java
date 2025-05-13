4c9c8c77620eca493bd785bd14c135a1ccee53ba
Slyfti <titout24@outlook.fr>
2025-05-13 09:46:04 +0200

    Ajout commentaires fonctions pas encore implémentées + run config vscode

--- src/cli/Main.java
+++ src/cli/Main.java
@@ -15,8 +15,8 @@ public class Main {
         try {
             switch (cmd) {
                 case "noise" -> runNoise(NoiseArgs.parse(rest));
-                case "denoise" -> runDenoise(DenoiseArgs.parse(rest));
-                case "eval" -> runEval(EvalArgs.parse(rest));
+                // case "denoise" -> runDenoise(DenoiseArgs.parse(rest));
+                // case "eval" -> runEval(EvalArgs.parse(rest));
                 case "--help", "-h" -> CliUtil.printGlobalHelp();
                 default -> throw new IllegalArgumentException("Commande inconnue : " + cmd);
             }
@@ -27,6 +27,6 @@ public class Main {
     }
 
     private static void runNoise(NoiseArgs a) { /* appel à core.Noiser */ }
-    private static void runDenoise(DenoiseArgs a) { /* appel à core.Denoiser */ }
-    private static void runEval(EvalArgs a) { /* appel à core.Evaluator */ }
+    // private static void runDenoise(DenoiseArgs a) { /* appel à core.Denoiser */ }
+    // private static void runEval(EvalArgs a) { /* appel à core.Evaluator */ }
 }
