fce7a218748ff7188ae4967cb3cee634ee7aaad9
Paul Cousin <p_cousin@outlook.fr>
2025-05-13 12:05:07 +0200

    PatchExtractor squelette

---
+++ src/PatchExtractor.java
@@ -0,0 +1,15 @@
+
+public class PatchExtractor {
+	public ArrayList<Patch> ExtractPatchs(Image img, int s) {
+		
+	}
+	public Image ReconstructPatchs(ArrayList<Patch> patchList, int l, int c) {
+		
+	}
+	public ArrayList<Image> DecoupeImage(Image img, int w, int n) {
+		
+	}
+	public ArrayList<int[]> VectorPatchs(ArrayList<Patch> patchList) {
+		
+	}
+}
